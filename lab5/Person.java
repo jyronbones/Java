dfc5d7c8808d4b2e55eb413d6630b2d134a5abe1
Byron <80455013+jyronbones@users.noreply.github.com>
2021-11-17 19:52:30 -0500

    Add files via upload

---
+++ lab5/Person.java
@@ -0,0 +1,65 @@
+package com.algonquincollege.cst8132.lab5;
+/*
+ * Assessment: Lab 5
+ * Student Name: Byron Jones
+ * Due Date: 11/14/21
+ * Description: A store management system that holds employee information and calculates salary
+ * Professor Name: Mike Norman
+ */
+/*
+ * This class stores and gets the information of a person
+ */
+/**
+ * This class stores and returns the information of a person
+ * @author Byron Jones
+ * @version 1.0
+ * @since 1.8
+ *
+ */
+public class Person {
+
+	/**
+	 * holds a person's first name
+	 */
+	protected String firstName; // represents first name of a person
+	/**
+	 * holds a person's last name
+	 */
+	protected String lastName; // represents last name of a person
+	/**
+	 * holds a person's email
+	 */
+	protected String email; // represents email of a person
+	/**
+	 * holds a person's phone number
+	 */
+	protected long phoneNumber; // represents phone number of a person
+	
+	/*
+	 * No-arg constructor
+	 */
+	/**
+	 * No-arg constructor
+	 */
+	public Person() {
+		super();
+	}
+	
+	/*
+	 * Parameterized constructor assigning information of a person, passed from Employee
+	 */
+	/**
+	 * Parameterized constructor assigning information of a person, passed from Employee
+	 * @param firstName is a person's first name
+	 * @param lastName is a person's last name
+	 * @param email is a person's email
+	 * @param phoneNumber is a person's phone number
+	 */
+	public Person(String firstName, String lastName,String email, long phoneNumber) {
+		this();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+	}
+}
---
+++ lab5/Regular.java
@@ -0,0 +1,79 @@
+package com.algonquincollege.cst8132.lab5;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+/*
+ * Assessment: Lab 5
+ * Student Name: Byron Jones
+ * Due Date: 11/14/21
+ * Description: A store management system that holds employee information and calculates salary
+ * Professor Name: Mike Norman
+ */
+/*
+ * This class contains methods to read and print a regular employee's personal information
+ */
+/**
+ * This class contains methods to read and print a regular employee's personal information
+ * @author Byron Jones
+ * @version 1.0
+ * @since 1.8
+ *
+ */
+public class Regular extends Employee{
+	/**
+	 * stores salary of a regular employee
+	 */
+	protected double salary; // stores salary of a regular employee
+	
+	/*
+	 * This method reads personal information of a regular employee
+	 */
+	/**
+	 * This method reads personal information of a regular employee
+	 * @param input is a Scanner used to allow input from user for information of a regular employee
+	 */
+	@Override
+	public void readEmployee(Scanner input) {
+		super.readEmployee(input); // calls the employee class readEmployee to read info of a regular employee
+		
+		// ask the user for annual salary
+		while (true) {
+			try {
+				System.out.print("Enter annual salary: ");
+				salary = input.nextDouble() / 12;
+				while (salary < 0) {
+					throw new IllegalArgumentException();
+				}
+				break;
+			} catch (InputMismatchException e) {
+				System.err.println("Input mismatch. Please enter a valid salary");
+				input.nextLine(); // clears buffer
+			} catch (IllegalArgumentException iae) {
+				System.err.println("Annual salary cannot be negative");
+				input.nextLine(); // clears buffer
+			}
+		}
+	}
+	
+	/*
+	 * This method is used to increase salary of a regular employee by 3%
+	 */
+	/**
+	 * This method is used to increase salary of a regular employee by 3%
+	 */
+	@Override
+	public void processIncrements() {
+		salary *= 1.03;
+	}
+	
+	/*
+	 * This method is used to print salary of a regular employee
+	 */
+	/**
+	 * This method is used to print salary of a regular employee
+	 */
+	@Override
+	public void printEmployee() {
+		super.printEmployee(); // calls employee class printEmployee
+		System.out.printf(" %12.2f |\n", salary);
+	}
+}
