872e71166e88e2b6adbdc25fa8af3ad4f116d7f0
Byron <80455013+jyronbones@users.noreply.github.com>
2021-12-16 22:55:00 -0500

    Update Policies.java

--- Library Management System/Policies.java
+++ Library Management System/Policies.java
@@ -6,9 +6,6 @@ package com.algonquin.cst8132.library;
  * Description: A library management system
  * Professor Name: Mike Norman
  */
-/*
- * This interface contains the field variables of overdue fine and max number of books for a member
- */
 /**
  * This interface contains the field variables of overdue fine and max number of books for a member
  * @author Byron Jones
@@ -16,16 +13,10 @@ package com.algonquin.cst8132.library;
  * @since 1.8
  */
 public interface Policies {
-	/*
-	 * stores overdue fine amount of a book
-	 */
 	/**
 	 * stores overdue fine amount of a book
 	 */
 	public static final double fine = 1.00;
-	/*
-	 * stores max number of books a member can rent at a time
-	 */
 	/**
 	 * stores max number of books a member can rent at a time
 	 */
